59e6142126bc4bca36aafea89643ba9903650f58
foxjazz <fox21@foxjazz.net>
2019-08-10 10:24:09 -0600

    updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SlowBuffer } from 'buffer';
+import { stringify } from 'querystring';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -21,9 +23,56 @@ export function activate(context: vscode.ExtensionContext) {
 
 		if (editor) {
 			let document = editor.document;
-
-			let line = document.getText();
-			let x = line.search("(");
+			let skip = false;
+			let startFn = 0;
+			let readText = document.getText();
+			let start = readText.indexOf("(");
+			while(start > 0){
+				let idx2 = start;
+				let idx = start;
+				let limit = 0;
+				while (idx2 > 0 && limit < 70)
+				{
+					limit++;
+					let data = readText[idx2];
+					let stdata = readText.substring(idx2,idx - idx2);
+					if (data === '.' || data === ')' || data === '}'){
+						skip = true;
+						break;
+					}
+					if(data === ' '){
+						startFn = idx2 + 1;
+						let rts = readText[idx2 - 1];
+						if(rts === '=' || rts === 'f' || rts === 'r' || rts === 'h' || rts === 'n'){
+							skip = true;
+						}
+						break;
+					}
+					idx2--;
+				}
+				let eol = readText.indexOf("\n", idx);
+				let restOf = readText.substring(start, eol - start);
+				if (restOf.indexOf("{") < 0){
+					skip = true;
+				}
+				if(skip){
+					start = readText.indexOf("(", eol);
+					skip = false;
+					continue;
+				}
+				let sta = readText.substring(startFn,idx - startFn);
+				let app = "console.log(\"" + sta + "\";\r\n";
+				let p = editor.document.positionAt(eol);	
+				editor.edit(editBuilder => {
+					
+					editBuilder.insert(p, app);
+					eol += app.length;
+					start = readText.indexOf("(", eol);
+				});
+				//readText = insert(readText,app,eol );
+				
+			
+			}
 			//let selection = editor.selection;
 
 			// Get the word within the selection
@@ -36,9 +85,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 		
 	});
-
+	
 	context.subscriptions.push(disposable);
 }
-
+function insert(base: string, argument: string, index: number): string{
+	const first = base.substring(0, index);
+	const second = base.substring(index, base.length - index);
+	return first + argument + second;
+}
 // this method is called when your extension is deactivated
 export function deactivate() {}
